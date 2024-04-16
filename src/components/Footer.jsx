98629a03fa73643ad4b8e355a7e40d7ff415abcd
tarunmalik330 <maliktarun2005@gmail.com>
2024-04-16 15:57:41 +0530

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
     <div className="bg-lightblack pt-[53px] pb-[63.34px]">
       <div className="container mx-auto xl:max-w-[1359px] px-3">
         <div className="flex flex-row items-center flex-wrap justify-between">
-          <div className="lg:w-3/12 sm:w-6/12 w-full max-sm:order-1">
-            <div className="flex gap-[77.64px]">
+          <div className="lg:w-3/12 w-full">
+            <div className="flex gap-[77.64px] max-lg:items-center">
               <a href="">
                 <img
                   src={pagelogo}
@@ -18,24 +18,28 @@ const Footer = () => {
               </a>
               <p className="text-lightgrey text-4sm font-medium tracking-8spacing">
                 {" "}
-                <span className="block">8fig, 1717 W 6th St,</span> Austin,
+                <span className="lg:block">8fig, 1717 W 6th St,</span> Austin,
                 Texas
               </p>
             </div>
           </div>
-          <div className="lg:w-3/12 sm:w-6/12">
-            <p className="text-center text-lightgrey tracking-8spacing font-medium text-4sm">
-              © 2024 8fig - All Rights Reserved
-            </p>
-          </div>
-          <div className="lg:w-3/12 sm:w-6/12 w-full max-sm:order-2 flex gap-[36px] items-baseline justify-end">
-            {footerlink.map((a, link) => {
-              return (
-                <div key={link}>
-                  <div>{a.footersvg}</div>
-                </div>
-              );
-            })}
+          <div className="lg:w-9/12">
+            <div className="flex flex-row flex-wrap justify-between">
+              <div className="lg:w-4/12 sm:w-6/12 w-full pt-4 lg:pt-0 flex">
+                <p className="text-center text-lightgrey tracking-8spacing font-medium text-4sm">
+                  © 2024 8fig - All Rights Reserved
+                </p>
+              </div>
+              <div className="lg:w-3/12 sm:w-6/12 w-full flex gap-[36px] items-baseline justify-end pt-4 lg:pt-0">
+                {footerlink.map((a, link) => {
+                  return (
+                    <div key={link}>
+                      <div>{a.footersvg}</div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
           </div>
         </div>
       </div>
